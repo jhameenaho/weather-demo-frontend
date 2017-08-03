79fc7dfebb6b27352515262d437dd6b9d2699c9a
Jaakko Hämeenaho <jaakko.hameenaho@symbio.com>
2017-08-03 16:52:20 +0300

    Revert "Switched controller params from temporary path to body"
    This reverts commit 2cbc39bbb68f6cb23bed92b686b98a7c4be3bafb.

--- backend/src/main/java/jhameenaho/weatherdemo/controller/WeatherController.java
+++ backend/src/main/java/jhameenaho/weatherdemo/controller/WeatherController.java
@@ -1,13 +1,11 @@
 package jhameenaho.weatherdemo.controller;
 
-import jhameenaho.weatherdemo.model.input.WeatherInput;
 import jhameenaho.weatherdemo.model.output.WeatherOutput;
 import jhameenaho.weatherdemo.model.weatherapi.Weather;
 import jhameenaho.weatherdemo.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,15 +22,15 @@ public class WeatherController {
         this.weatherService = weatherService;
     }
     
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/{query}", method = RequestMethod.GET, produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
-    public WeatherOutput getWeather(@RequestBody WeatherInput weather) {
-        return weatherService.getWeather(weather.getQuery());
+    public WeatherOutput getWeather(@PathVariable("query") String query) {
+        return weatherService.getWeather(query);
     }
     
-    @RequestMapping(value = "/full", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/{query}/full", method = RequestMethod.GET, produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
-    public Weather getWeatherFull(@RequestBody WeatherInput weather) {
-        return weatherService.getWeatherData(weather.getQuery());
+    public Weather getWeatherFull(@PathVariable("query") String query) {
+        return weatherService.getWeatherData(query);
     }
 }
